618cee2a82ddabb432abb14cbc6c4646add02493
Felipe Chernicharo <string7dev@gmail.com>
2022-02-22 22:28:38 -0500

    yup

--- src/components/DonorAccountForm/index.tsx
+++ src/components/DonorAccountForm/index.tsx
@@ -6,41 +6,60 @@ import {
 	Field,
 	FieldProps,
 } from "formik";
-import { useEffect } from "react";
+import * as yup from 'yup';
 import { DonorAccount, initialValues } from "../../utils/constants";
 import SlideToggle from "../shared/SlideToggle";
 import TextField from "../shared/TextField";
 import "./index.css";
 
+let schema = yup.object().shape({
+	firstName: yup.string(),
+	lastName: yup.string(),
+	company: yup.string(),
+	email: yup.string().email().required(),
+	isCompany: yup.boolean().required(),
+	phone: yup.number().required()
+})
+
 const DonorAccountForm = () => {
-	const handleSubmit = (values: DonorAccount, actions: FormikHelpers<DonorAccount>) => {
-		let submitData;
+	const handleSubmit = async (values: DonorAccount, actions: FormikHelpers<DonorAccount>) => {
+		console.log({ values, actions });
 
-		if (values.isCompany) {
-			submitData = { ...values, firstName: "", lastName: "" }
-		} else {
-			submitData = { ...values, company: "" };
-		}
-		console.log({ values, submitData, actions });
+		schema.validate(values).then(result => {
+			let submitData;
 
-		actions.setSubmitting(true)
+			if (values.isCompany)
+				submitData = { ...values, firstName: "", lastName: "" }
+			else
+				submitData = { ...values, company: "" };
 
-		alert(JSON.stringify(submitData, null, 2));
-		setTimeout(() => actions.setSubmitting(false), 600);
+
+
+			actions.setSubmitting(true)
+
+			alert(JSON.stringify(submitData, null, 2));
+			setTimeout(() => actions.setSubmitting(false), 600);
+			console.log({ result })
+
+		}).catch(err => {
+			console.log({ err })
+			alert(JSON.stringify(err, null, 2));
+
+		})
+
 	};
 
 	return (
 		<>
 			<h2>
-				<>Donor Account </>
+				<>Donor Account</>
 			</h2>
 
 			<Formik
 				initialValues={initialValues.DONOR_ACCOUNT}
 				onSubmit={(values, actions) => handleSubmit(values, actions)}
 			>
-				{({ isSubmitting, isValidating, values, handleChange, unregisterField }) => {
-
+				{({ values, isValidating, isSubmitting }: FormikProps<DonorAccount>) => {
 					return (
 						<Form className="form">
 							<SlideToggle
@@ -58,7 +77,7 @@ const DonorAccountForm = () => {
 								<TextField id="company" name="company" placeholder="Company" />
 							)}
 							<TextField id="email" name="email" placeholder="Email" />
-							<TextField id="phone" name="phone" placeholder="Phone" />
+							<TextField id="phone" name="phone" type="tel" placeholder="555-5555" />
 							<TextField id="address" name="address" placeholder="Address" />
 
 							<button
--- src/components/shared/TextField/index.tsx
+++ src/components/shared/TextField/index.tsx
@@ -1,17 +1,18 @@
-import { Field } from "formik";
+import { Field, FieldProps, FormikProps } from "formik";
 import { capitalize } from '../../../utils/functions'
 
 interface Props {
   id: string,
   name: string,
+  type?: string,
   placeholder?: string,
 }
 
-const TextField = ({ name, id, placeholder }: Props) => {
+const TextField = ({ name, id, placeholder, type }: Props) => {
   return (
     <>
       <label htmlFor={name}>{capitalize(name)}</label>
-      <Field id={id} name={name} placeholder={placeholder} />
+      <Field id={id} type={type || 'text'} name={name} placeholder={placeholder} />
     </>
   );
 };
